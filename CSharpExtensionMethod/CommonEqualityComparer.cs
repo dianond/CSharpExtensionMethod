16203d576762ce55cc6f0e0981404fb4c30c4c1a
dianond <dianond@qq.com>
2016-05-21 14:52:56 +0800

    Distinct扩展方法

---
+++ CSharpExtensionMethod/CommonEqualityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpExtensionMethod
+{
+    public class CommonEqualityComparer<T, V> : IEqualityComparer<T>
+    {
+        private Func<T, V> keySelector;
+        private IEqualityComparer<V> comparer;
+
+        public CommonEqualityComparer(Func<T, V> keySelector, IEqualityComparer<V> comparer = null)
+        {
+            this.keySelector = keySelector;
+            this.comparer = comparer ?? EqualityComparer<V>.Default;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return comparer.Equals(keySelector(x), keySelector(y));
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return comparer.GetHashCode(keySelector(obj));
+        }
+    }
+}
---
+++ CSharpExtensionMethodUnitTest/DistinctExtensionsUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using CSharpExtensionMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpExtensionMethodUnitTest
+{
+    [TestClass]
+    public class DistinctExtensionsUnitTest
+    {
+        [TestMethod]
+        public void TestDistinct()
+        {
+            var data1 = new[]
+            {
+                new Person {ID = 1, Name = "001"},
+                new Person {ID = 1, Name = "002"}
+            };
+            var ps1 = data1
+                .Distinct(p => p.ID)
+                .ToArray();
+            Assert.AreEqual(1, ps1.Length);
+        }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
